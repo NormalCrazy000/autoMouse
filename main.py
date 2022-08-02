71192f312700a83225581e0834487b379cdc6e8b
gabriele-giannotto <gabriele.giannotto@mail.polimi.it>
2022-08-03 01:19:56 +0200

    Add function to set path and to start movement Now you can create a path with nodes. On nodes mouses will be clicked

--- main.py
+++ main.py
@@ -6,22 +6,35 @@ import mouse
 
 position = []
 i = 0
-
 other = True
-mouse.on_right_click(lambda: print(mouse.get_position()))
-while 1:
-    1
-'''
+
+
+def setPosition():
+    global other
+    if other:
+
+        print("Position saved: " + str(mouse.get_position()[0]) + "," + str(mouse.get_position()[1]))
+        position.append(mouse.get_position()[0])
+        position.append(mouse.get_position()[1])
+        position.append(input("time: "))
+        temp = input("Other movement(Y/N): ")
+        if temp == 'N':
+            other = False
+
+
+mouse.on_middle_click(lambda: setPosition())
 while other:
-    position.append(input("Position x: "))
-    position.append(input("Position Y: "))
-    position.append(input("time: "))
-    temp = input("Other movement(Y/N): ")
-    if temp == 'N':
-        other = False
-j = 0
-while j < len(position):
-    mouse.move(position[j], position[j + 1], absolute=False, duration=0.2)
-    time.sleep(float(position[j + 2]))
-    j = j + 3
-'''
+    if other == False:
+        break;
+k = True
+time.sleep(5)
+while (k < 10):
+    j = 0
+    while j < len(position):
+        mouse.move(position[j] - mouse.get_position()[0], position[j + 1] - mouse.get_position()[1], absolute=False,
+                   duration=0.2)
+        mouse.click()
+        time.sleep(float(position[j + 2]))
+        j = j + 3
+    time.sleep(30)
+    k = k + 1
